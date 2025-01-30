15e2508a42cfc9aae8ef6b6787c244ff8f103a2c
Anton Trochuk <118171581+AntonTrochuk@users.noreply.github.com>
2025-01-30 17:36:00 +0400

    Add files via upload

--- send.php
+++ send.php
@@ -9,17 +9,17 @@ if (!error_get_last()) {
 
     // Переменные, которые отправляет пользователь
     $name = $_POST['name'] ;
-    //$email = $_POST['email'];
+    $tel = $_POST['tel'];
     //$text = $_POST['text'];
     //$file = $_FILES['myfile'];
 
 
     // Формирование самого письма
-    $title = "Заголовок письма";
+    $title = "Сайт Типография Врамке";
     $body = "
     <h2>Новое письмо</h2>
     <b>Имя:</b> $name<br>
-    <b>Почта:</b> $email<br><br>
+    <b>Телефон:</b> $tel<br><br>
     <b>Сообщение:</b><br>$text
     ";
 
