d96746f6c320a675b1b7043c0a7034d738395de1
Anton Trochuk <trochuk19@gmail.com>
2025-02-20 17:55:41 +0400

    добавление интерактивности в модальном окне

--- contact-send.js
+++ contact-send.js
@@ -1,33 +1,43 @@
-
-
 async function submitForm(event) {
-	event.preventDefault(); // отключаем перезагрузку/перенаправление страницы
+	event.preventDefault();
 	try {
-		// Формируем запрос
 		const response = await fetch(event.target.action, {
 			method: 'POST',
 			body: new FormData(event.target)
 		});
-		// проверяем, что ответ есть
+
 		if (!response.ok) throw (`Ошибка при обращении к серверу: ${response.status}`);
-		// проверяем, что ответ действительно JSON
+
 		const contentType = response.headers.get('content-type');
 		if (!contentType || !contentType.includes('application/json')) {
 			throw ('Ошибка обработки. Ответ не JSON');
 		}
-		// обрабатываем запрос
+
 		const json = await response.json();
 		if (json.result === "success") {
-			// в случае успеха
-			const modalWindowDOMElement = document.querySelector('.modal-window');
-			modalWindowDOMElement.classList.add('modal-window--close');
+			closeModal();
 			alert(json.info);
 		} else {
-			// в случае ошибки
+
 			console.log(json);
 			throw (json.info);
 		}
-	} catch (error) { // обработка ошибки
+	} catch (error) {
 		alert(error);
 	}
 }
+
+function closeModal() {
+	const modalWindowDOMElement = document.querySelector('.modal-window');
+	if (modalWindowDOMElement) {
+		modalWindowDOMElement.classList.add('modal-window--close');
+	} else {
+		console.error("Модальное окно не найдено");
+	}
+}
+
+document.addEventListener('keydown', (event) => {
+	if (event.key === 'Escape') {
+		closeModal();
+	}
+});
